f5360099632c83ad2c02bdad113c7339d4fecc62
Arjun Kahlon <arjunskahlon@gmail.com>
2022-12-16 13:06:16 -0800

    Removed unnecessary CSS that came with create-react-app template

--- top100/src/App.js
+++ top100/src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import NavigationBar from './components/nav-bar'; 
 import AlbumList from './components/album-list';
-import './app.css';
 import PageContainer from './components/page-container'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
